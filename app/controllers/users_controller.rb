070a2a6c138209c940ee5417ebcb018ad73e13bf
SamanthaMGarcia <samantha.marie.garcia@gmail.com>
2018-07-20 16:39:37 -0400

    add create and new methods to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,12 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(params.require(:user).permit(:username, :password)
+    @user.save
+    redirect_to user_path(@user)
   end
 end
