1a34bc3536c9d94ba54614137608f957579d25fd
SamanthaMGarcia <samantha.marie.garcia@gmail.com>
2018-07-29 17:33:41 -0400

    add activerecord validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_many :requests
   has_many :days, through: :requests
   has_secure_password
+  validates :username, uniqueness: true
+  validates :username, presence: true
 end
