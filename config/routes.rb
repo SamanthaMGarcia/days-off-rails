4d0cce963d2ee94beb5249df8deb586c49c225b8
SamanthaMGarcia <samantha.marie.garcia@gmail.com>
2018-07-30 14:34:34 -0400

    add logout funcationality

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,11 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   get '/auth/facebook/callback', to: 'sessions#create'
- root "application#home"
+  root "application#home"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
