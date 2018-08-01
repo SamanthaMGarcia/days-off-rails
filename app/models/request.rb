0e888ea9e21611361d057f6a7e76c3e0246e03b0
SamanthaMGarcia <samantha.marie.garcia@gmail.com>
2018-08-01 18:50:16 -0400

    remove spaces

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,10 +3,14 @@ class Request < ApplicationRecord
   belongs_to :day
 
   def day_attributes=(day_hash)
-    ymd = "#{day_hash["dayoff(1i)"]} - #{day_hash["dayoff(2i)"]} - #{day_hash["dayoff(3i)"]}"
+    ymd = "#{day_hash["dayoff(1i)"]}-#{day_hash["dayoff(2i)"]}-#{day_hash["dayoff(3i)"]}"
     self.day = Day.find_or_create_by(:dayoff => ymd)
     self.save
   end
+
+  # def ymd
+  #   ymd = "#{day_hash["dayoff(1i)"]} - #{day_hash["dayoff(2i)"]} - #{day_hash["dayoff(3i)"]}"
+  # end
 end
 
 
