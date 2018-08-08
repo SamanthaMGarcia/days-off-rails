c68a81a6d3d7eccb429a9b982723c7c41e55683b
SamanthaMGarcia <samantha.marie.garcia@gmail.com>
2018-08-08 10:09:17 -0400

    refactor

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,10 +2,11 @@ class RequestsController < ApplicationController
   before_action :access_allowed
 
   def create
+    @user = User.find_by(id: params[:id])
     @request = Request.new(request_params)
 
     if @request.save
-      redirect_to user_path(request.user)
+      redirect_to user_path(current_user)
     else
        flash.now[:error] = @request.errors.full_messages
        render :new
@@ -45,5 +46,12 @@ class RequestsController < ApplicationController
   private
     def request_params
       params.require(:request).permit(:user_id, :day_id, :reason, day_attributes:[:dayoff])
+    end
+    
+    def access_allowed
+      @request = Request.find(params[:id])
+      if @request.user != current_user
+        redirect_to user_path
+      end
     end
 end
