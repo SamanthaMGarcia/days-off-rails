c9f320203775c2fba1f601b93f5802a20bcdc0c8
SamanthaMGarcia <samantha.marie.garcia@gmail.com>
2018-08-07 20:30:31 -0400

    create access_allowed method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
-def home
- end
-
  def current_user
    @current_user ||= User.find_by(id: session[:user_id])
  end
@@ -11,4 +8,15 @@ def home
  def logged_in?
    !!current_user
  end
+
+ def require_login
+    return head(:forbidden) unless session.include? :user_id
+  end
+
+  def access_allowed
+	   @request = Request.find(params[:id])
+      if @request.user != current_user
+        redirect_to user_path
+      end
+  end
 end
