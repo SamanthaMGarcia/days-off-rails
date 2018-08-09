7ab3453429b00f2d0928b7fe2217c1419f811438
SamanthaMGarcia <samantha.marie.garcia@gmail.com>
2018-08-09 16:49:42 -0400

    title table migrated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :requests
   has_many :days, through: :requests
+  has_many :titles
   has_secure_password
   validates :username, uniqueness: true
   validates :username, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 4) do
 
   create_table "days", force: :cascade do |t|
     t.date "dayoff"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 3) do
     t.integer "day_id"
     t.integer "user_id"
     t.string "reason"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "titles", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
